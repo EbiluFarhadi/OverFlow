b257de6973d69ba36949511d02d249f70a75ccb3
ebilu farhadi <ebilufarhadi@gmail.com>
2024-03-13 00:56:19 +0330

    cloning myOverFlow

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const { hostname } = require("os");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+    swcMinify: false, // it should be false by default
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
